dd2d84bc63f124a6a2613a11f833efd8c7cea572
bmalinowsky <bmalinowsky@github.com>
2014-08-20 21:37:16 +0200

    Add monitor command option, documentation of DPT aliases, cleanup

--- src/tuwien/auto/calimero/tools/ProcComm.java
+++ src/tuwien/auto/calimero/tools/ProcComm.java
@@ -42,10 +42,14 @@ import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
+import tuwien.auto.calimero.DataUnitBuilder;
+import tuwien.auto.calimero.DetachEvent;
 import tuwien.auto.calimero.GroupAddress;
 import tuwien.auto.calimero.Settings;
 import tuwien.auto.calimero.datapoint.Datapoint;
 import tuwien.auto.calimero.datapoint.StateDP;
+import tuwien.auto.calimero.dptxlator.DPTXlator;
+import tuwien.auto.calimero.dptxlator.TranslatorTypes;
 import tuwien.auto.calimero.exception.KNXException;
 import tuwien.auto.calimero.exception.KNXFormatException;
 import tuwien.auto.calimero.exception.KNXIllegalArgumentException;
@@ -64,7 +68,9 @@ import tuwien.auto.calimero.log.LogStreamWriter;
 import tuwien.auto.calimero.log.LogWriter;
 import tuwien.auto.calimero.process.ProcessCommunicator;
 import tuwien.auto.calimero.process.ProcessCommunicatorImpl;
+import tuwien.auto.calimero.process.ProcessEvent;
 import tuwien.auto.calimero.process.ProcessListener;
+import tuwien.auto.calimero.process.ProcessListenerEx;
 
 /**
  * A tool for Calimero 2 providing basic process communication.
@@ -152,41 +158,44 @@ public class ProcComm implements Runnable
 	/**
 	 * Entry point for running ProcComm.
 	 * <p>
-	 * An IP host or port identifier has to be supplied, specifying the endpoint for the
-	 * KNX network access.<br>
-	 * To show the usage message of this tool on the console, supply the command line
-	 * option -help (or -h).<br>
-	 * Command line options are treated case sensitive. Available options for the
-	 * communication connection:
+	 * An IP host or port identifier has to be supplied, specifying the endpoint for the KNX network
+	 * access.<br>
+	 * To show the usage message of this tool on the console, supply the command line option -help
+	 * (or -h).<br>
+	 * Command line options are treated case sensitive. Available options for the communication
+	 * connection:
 	 * <ul>
 	 * <li><code>-help -h</code> show help message</li>
 	 * <li><code>-version</code> show tool/library version and exit</li>
 	 * <li><code>-verbose -v</code> enable verbose status output</li>
 	 * <li><code>-localhost</code> <i>id</i> &nbsp;local IP/host name</li>
-	 * <li><code>-localport</code> <i>number</i> &nbsp;local UDP port (default system
-	 * assigned)</li>
+	 * <li><code>-localport</code> <i>number</i> &nbsp;local UDP port (default system assigned)</li>
 	 * <li><code>-port -p</code> <i>number</i> &nbsp;UDP port on host (default 3671)</li>
 	 * <li><code>-nat -n</code> enable Network Address Translation</li>
 	 * <li><code>-routing</code> use KNXnet/IP routing</li>
 	 * <li><code>-serial -s</code> use FT1.2 serial communication</li>
-	 * <li><code>-medium -m</code> <i>id</i> &nbsp;KNX medium [tp0|tp1|p110|p132|rf]
-	 * (defaults to tp1)</li>
+	 * <li><code>-medium -m</code> <i>id</i> &nbsp;KNX medium [tp0|tp1|p110|p132|rf] (defaults to
+	 * tp1)</li>
 	 * </ul>
 	 * Available commands for process communication:
 	 * <ul>
-	 * <li><code>read</code> <i>DPT &nbsp;KNX-address</i> &nbsp;read from group address,
+	 * <li><code>read</code> <i>DPT &nbsp;KNX-address</i> &nbsp;read from group address, using DPT
+	 * value format</li>
+	 * <li><code>write</code> <i>DPT &nbsp;value &nbsp;KNX-address</i> &nbsp;write to group address,
 	 * using DPT value format</li>
-	 * <li><code>write</code> <i>DPT &nbsp;value &nbsp;KNX-address</i> &nbsp;write to
-	 * group address, using DPT value format</li>
+	 * <li><code>monitor</code> enter group monitoring, can also be used together with the
+	 * <code>read</code> or <code>write</code> command</li>
 	 * </ul>
-	 * For the more common datapoint types (DPTs) the following name aliases can be used
-	 * instead of the general DPT number string:
+	 * For the more common datapoint types (DPTs) the following name aliases can be used instead of
+	 * the general DPT number string:
 	 * <ul>
 	 * <li><code>switch</code> for DPT 1.001</li>
 	 * <li><code>bool</code> for DPT 1.002</li>
 	 * <li><code>string</code> for DPT 16.001</li>
-	 * <li><code>float</code> for DPT 9.002</li>
+	 * <li><code>float</code> or <code>float2</code> for DPT 9.002</li>
+	 * <li><code>float4</code> for DPT 14.005</li>
 	 * <li><code>ucount</code> for DPT 5.010</li>
+	 * <li><code>int</code> for DPT 13.001</li>
 	 * <li><code>angle</code> for DPT 5.003</li>
 	 * </ul>
 	 *
@@ -207,7 +216,7 @@ public class ProcComm implements Runnable
 			sh.unregister();
 		}
 		catch (final Throwable t) {
-			out.error("parsing options", t);
+			out.error("tool options", t);
 		}
 		LogManager.getManager().shutdown(true);
 	}
@@ -222,6 +231,12 @@ public class ProcComm implements Runnable
 		try {
 			start(null);
 			readWrite();
+			if (options.containsKey("monitor")) {
+				synchronized (this) {
+					while (true)
+						wait();
+				}
+			}
 		}
 		catch (final KNXException e) {
 			thrown = e;
@@ -268,14 +283,20 @@ public class ProcComm implements Runnable
 
 		// create the network link to the KNX network
 		final KNXNetworkLink lnk = createLink();
-		// ??? if this is giving useful output, re-enable lnk logging
-		//if (w != null)
-		//	LogManager.getManager().addWriter(lnk.getName(), w);
-
 		// create process communicator with the established link
 		pc = new ProcessCommunicatorImpl(lnk);
 		if (l != null)
 			pc.addProcessListener(l);
+
+		// this is the listener if group monitoring is requested
+		final ProcessListener monitor = new ProcessListenerEx(){
+			public void groupWrite(ProcessEvent e) { onGroupEvent(e); }
+			public void groupReadResponse(ProcessEvent e) { onGroupEvent(e); }
+			public void groupReadRequest(ProcessEvent e) { onGroupEvent(e); }
+			public void detached(DetachEvent e) {}
+		};
+		pc.addProcessListener(monitor);
+
 		// user might specify a response timeout for KNX message
 		// answers from the KNX network
 		if (options.containsKey("timeout"))
@@ -296,6 +317,28 @@ public class ProcComm implements Runnable
 	}
 
 	/**
+	 * Called by this tool on receiving a process communication group event.
+	 * <p>
+	 *
+	 * @param e the process event
+	 */
+	protected void onGroupEvent(final ProcessEvent e)
+	{
+		final byte[] asdu = e.getASDU();
+		String s = e.getSourceAddr() + "->" + e.getDestination() + " "
+				+ DataUnitBuilder.decodeAPCI(e.getServiceCode()) + " "
+				+ DataUnitBuilder.toHex(asdu, " ");
+		final GroupAddress dst = (GroupAddress) options.get("dst");
+		try {
+			if (e.getDestination().equals(dst) && asdu.length > 0)
+				s = s + " (" + asString(asdu, 0, getDPT()) + ")";
+		}
+		catch (final KNXException ke) {}
+		catch (final KNXIllegalArgumentException iae) {}
+		out.log(LogLevel.ALWAYS, s, null);
+	}
+
+	/**
 	 * Called by this tool on completion.
 	 * <p>
 	 *
@@ -309,6 +352,25 @@ public class ProcComm implements Runnable
 			out.info("process communicator was stopped");
 		if (thrown != null)
 			out.error("completed", thrown);
+	}
+
+	/**
+	 * Returns a string translation of the datapoint data for the specified datapoint type, using
+	 * the process event ASDU.
+	 * <p>
+	 *
+	 * @param asdu the process event ASDU with the datapoint data
+	 * @param dptMainNumber DPT main number &ge; 0, can be 0 if the <code>dptID</code> is unique
+	 * @param dptID datapoint type ID to lookup the translator
+	 * @return the datapoint value
+	 * @throws KNXException
+	 */
+	protected String asString(final byte[] asdu, final int dptMainNumber, final String dptID)
+		throws KNXException
+	{
+		final DPTXlator t = TranslatorTypes.createTranslator(dptMainNumber, dptID);
+		t.setData(asdu);
+		return t.getValue();
 	}
 
 	/**
@@ -354,44 +416,42 @@ public class ProcComm implements Runnable
 	private String getDPT()
 	{
 		final String dpt = (String) options.get("dpt");
-		if (dpt.equals("switch"))
+		if ("switch".equals(dpt))
 			return "1.001";
-		if (dpt.equals("bool"))
+		if ("bool".equals(dpt))
 			return "1.002";
-		if (dpt.equals("string"))
+		if ("string".equals(dpt))
 			return "16.001";
-		if (dpt.equals("float"))
+		if ("float".equals(dpt))
 			return "9.002";
-		if (dpt.equals("ucount"))
-			return "5.010";
-		if (dpt.equals("int"))
-			return "13.001";
-		if (dpt.equals("float32"))
+		if ("float2".equals(dpt))
+			return "9.002";
+		if ("float4".equals(dpt))
 			return "14.005";
-		if (dpt.equals("angle"))
+		if ("ucount".equals(dpt))
+			return "5.010";
+		if ("int".equals(dpt))
+			return "13.001";
+		if ("angle".equals(dpt))
 			return "5.003";
 		return dpt;
 	}
 
 	private void readWrite() throws KNXException, InterruptedException
 	{
-		// check if we are doing a read or write operation
-		final boolean read = options.containsKey("read");
-		final GroupAddress main = (GroupAddress) options.get(read ? "read" : "write");
+		final GroupAddress main = (GroupAddress) options.get("dst");
 		// encapsulate information into a datapoint
 		// this is a convenient way to let the process communicator
 		// handle the DPT stuff, so an already formatted string will be returned
 		final Datapoint dp = new StateDP(main, "", 0, getDPT());
-		final String s;
-		if (read)
-			s = "read value: " + pc.read(dp);
-		else {
+		if (options.containsKey("read"))
+			out.log(LogLevel.ALWAYS, "read value: " + pc.read(dp), null);
+		if (options.containsKey("write")) {
 			// note, a write to a non existing datapoint might finish successfully,
 			// too.. no check for existence or read back of a written value is done
 			pc.write(dp, (String) options.get("value"));
-			s = "write successful";
+			out.log(LogLevel.ALWAYS, "write successful", null);
 		}
-		out.info(s);
 	}
 
 	/**
@@ -430,9 +490,10 @@ public class ProcComm implements Runnable
 			else if (isOption(arg, "read", null)) {
 				if (i + 2 >= args.length)
 					break;
+				options.put("read", null);
 				options.put("dpt", args[++i]);
 				try {
-					options.put("read", new GroupAddress(args[++i]));
+					options.put("dst", new GroupAddress(args[++i]));
 				}
 				catch (final KNXFormatException e) {
 					throw new KNXIllegalArgumentException("read DPT: " + e.getMessage(), e);
@@ -441,15 +502,18 @@ public class ProcComm implements Runnable
 			else if (isOption(arg, "write", null)) {
 				if (i + 3 >= args.length)
 					break;
+				options.put("write", null);
 				options.put("dpt", args[++i]);
 				options.put("value", args[++i]);
 				try {
-					options.put("write", new GroupAddress(args[++i]));
+					options.put("dst", new GroupAddress(args[++i]));
 				}
 				catch (final KNXFormatException e) {
 					throw new KNXIllegalArgumentException("write DPT: " + e.getMessage(), e);
 				}
 			}
+			else if (isOption(arg, "monitor", null))
+				options.put("monitor", null);
 			else if (isOption(arg, "-localhost", null))
 				parseHost(args[++i], true, options);
 			else if (isOption(arg, "-localport", null))
@@ -476,8 +540,11 @@ public class ProcComm implements Runnable
 		}
 		if (options.containsKey("host") == options.containsKey("serial"))
 			throw new KNXIllegalArgumentException("no host or serial port specified");
-		if (options.containsKey("read") == options.containsKey("write"))
-			throw new KNXIllegalArgumentException("do either read or write");
+		if (!(options.containsKey("monitor") || options.containsKey("read") || options
+				.containsKey("write")))
+			throw new KNXIllegalArgumentException("specify read, write, or group monitoring");
+		if (options.containsKey("read") && options.containsKey("write"))
+		throw new KNXIllegalArgumentException("either read or write - not both");
 	}
 
 	private static void showUsage()
@@ -501,9 +568,12 @@ public class ProcComm implements Runnable
 		sb.append("Available commands for process communication:").append(sep);
 		sb.append("  read <DPT> <KNX address>           read from group address").append(sep);
 		sb.append("  write <DPT> <value> <KNX address>  write to group address").append(sep);
+		sb.append("  monitor                 enter group monitoring, can also be").append(sep)
+		  	.append("                          used together with read or write ").append(sep);
 		sb.append("Additionally recognized name aliases for DPT numbers:").append(sep);
-		sb.append("  switch (1.001), bool (1.002), string (16.001)").append(sep)
-				.append("  float (9.002), ucount (5.010), angle (5.003)");
+		sb.append("  switch (1.001), bool (1.002), string (16.001), float/float2 (9.002)")
+				.append(sep)
+				.append("  float4 (14.005), ucount (5.010), int (13.001), angle (5.003)");
 		out.log(LogLevel.ALWAYS, sb.toString(), null);
 	}
 
