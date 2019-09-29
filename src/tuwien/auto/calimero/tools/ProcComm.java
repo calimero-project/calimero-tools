ec82c21cf36816f7ff8377564b10b7cb31df5d29
bmalinowsky <bmalinowsky@github.com>
2019-09-29 21:43:32 +0200

    Support lookup of default usb interface

--- src/tuwien/auto/calimero/tools/ProcComm.java
+++ src/tuwien/auto/calimero/tools/ProcComm.java
@@ -892,6 +892,10 @@ public class ProcComm implements Runnable
 			else
 				throw new KNXIllegalArgumentException("unknown option " + arg);
 		}
+		// we allow a default usb config where the first knx usb device is used
+		if (options.containsKey("usb") && !options.containsKey("host"))
+			options.put("host", "");
+
 		if (!options.containsKey("host") || (options.containsKey("ft12") && options.containsKey("usb")))
 			throw new KNXIllegalArgumentException("specify either IP host, serial port, or device");
 		if (!(options.containsKey("monitor") || options.containsKey("read") || options.containsKey("write") || options.containsKey("info")))
