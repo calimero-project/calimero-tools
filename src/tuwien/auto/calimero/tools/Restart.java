de086a56863d2483a991205cba093dca89e45d4e
bmalinowsky <bmalinowsky@github.com>
2020-02-28 18:51:44 +0100

    Distinguish I/O interruption from cancelation

--- src/tuwien/auto/calimero/tools/Restart.java
+++ src/tuwien/auto/calimero/tools/Restart.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2019 B. Malinowsky
+    Copyright (c) 2019, 2020 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -153,8 +153,12 @@ public class Restart implements Runnable {
 			thrown = e;
 		}
 		catch (final InterruptedException e) {
-			canceled = true;
 			Thread.currentThread().interrupt();
+			final var msg = e.getMessage();
+			if (msg != null)
+				out(msg);
+			else
+				canceled = true;
 		}
 		finally {
 			onCompletion(thrown, canceled);
