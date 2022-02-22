872bceb9a0890c1ad4e3afb090c8f4c2fa6bfbcd
bmalinowsky <bmalinowsky@github.com>
2022-02-22 20:05:41 +0100

    Use KNXIllegalArgumentException

--- src/tuwien/auto/calimero/tools/BaosClient.java
+++ src/tuwien/auto/calimero/tools/BaosClient.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2019, 2021 B. Malinowsky
+    Copyright (c) 2019, 2022 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -435,7 +435,7 @@ public class BaosClient implements Runnable
 		case "all": return ValueFilter.All;
 		case "valid": return ValueFilter.ValidOnly;
 		case "updated": return ValueFilter.UpdatedOnly;
-		default: throw new IllegalArgumentException("unknown value filter " + arg);
+		default: throw new KNXIllegalArgumentException("unknown value filter " + arg);
 		}
 	}
 
